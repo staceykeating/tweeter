d01c72fd7e8bdfdf4c375180567da1c818430117
Stacey Keating <staceyamberkk@gmail.com>
2020-04-23 20:36:34 +0000

    fix tweet header display

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,14 +11,13 @@ const escape = function (str) {
   return div.innerHTML;
 }
 
-
 const createTweetElement = function(data) {
   let $tweet = `
    <article class= "boxed"> 
     <header>
-      <img src="${escape(data.user.avatars)}"><h2>${escape(data.user.name)}</h2><h4>${escape(data.user.handle)}</h4>
+    <h2><img src="${escape(data.user.avatars)}">${escape(data.user.name)}</h2><h5 id="handle">${escape(data.user.handle)}</h5>
     </header>
-    <p>${escape(data.content.text)}</p>
+    <p>${escape(data.content.text)}</p> 
     <footer>
       <p>${escape(data.created_at)}</p><p><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></p>
     </footer>
@@ -37,27 +36,42 @@ const renderTweets = function(data) {
 
 //const $tweet = createTweetElement(data);
 
-
-$('form').submit(function(event) {
-  //alert('Hello');
-  event.preventDefault()  
-  //const tweet = data.content.text;
-  const self = this;
-  const formData = $(this).serialize(); 
-
-    $.post('/tweets', formData)
-    .then(() => {
-      $(self)[0].reset();
-      renderTweets(formData);
-    });
-}); 
-loadTweets();
-
 const loadTweets = function() {
   $.getJSON('/tweets')
   .then((formData) => {
     renderTweets(formData);
   }); 
 };
+
+$('form').submit(function(event) {
+  event.preventDefault()  
+  
+  const self = this;
+  const formData = $(this).serialize(); 
+  const tweetText = $('#tweet-text').val();
+  const tweetLength = tweetText.length
+  console.log(tweetLength);
+
+  if (tweetLength <= 140 && tweetLength > 0) {
+
+    $.post('/tweets', formData)
+    .then(() => {
+      $('.minExceed').hide(); 
+      $('.maxExceed').hide(); 
+      $(self)[0].reset();
+      loadTweets();
+    });
+  } 
+  else if (tweetLength > 140) {
+    $.post('/tweets')
+    $('.maxExceed').show(); 
+  }
+  else if (tweetLength < 1) {
+  $.post('/tweets')
+  $('.minExceed').show(); 
+  
+  }
+}); 
 loadTweets();
+
 });
