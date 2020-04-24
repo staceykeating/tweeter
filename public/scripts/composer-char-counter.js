6aac543708d27704808bfd03c64da209221b6c67
Stacey Keating <staceyamberkk@gmail.com>
2020-04-24 03:35:44 +0000

    lint

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
     const characters = 140;
     const counter = $(this).closest("section.new-tweet").find("#counter");
     const length = $(this).val().length;
-    const remaining = characters - length;  
+    const remaining = characters - length;
     counter.text(remaining);
     if (remaining < 0) {
-      counter.addClass("negative")
+      counter.addClass("negative");
     } else if (remaining > 0) {
       counter.removeClass("negative");
     }
